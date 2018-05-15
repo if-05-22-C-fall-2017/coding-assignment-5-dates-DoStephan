fa87d8368350bd2681637bd02c8fc4f6c4b95f4d
Stephan Do <stephan.do08@gmail.com>
2018-05-15 23:24:45 +0200

    finish homework 'germanDate'

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,6 +3,10 @@ import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
     public static Matcher getMatcher(String dateString) {
-        return null;
+        Pattern p;
+        p = Pattern.compile("^(0?[1-9]|1[0-9]|2[0-9]|3[0-1])(\\.|-|\\ )"
+                + "(1[0-2]|0?[1-9])(\\.|-|\\ )(19|[2-9][0-9])?\\d{2}$");
+        Matcher m = p.matcher(dateString);
+        return m;
     }
 }
